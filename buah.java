c7f5ebff530b91d48cde500d78bf6fad22e8d078
elsalatipatulsadiah <elsalatipatulsadiah@gmail.com>
2024-11-16 13:08:06 +0700

    Add files via upload

---
+++ buah.java
@@ -0,0 +1,18 @@
+
+public String binatang {
+public static void main(String[] args) {
+        String hewan = "sapi";
+        System.out.println(hewan.length());
+    }
+}
+
+
+public class buah{
+    public static void main(String[] args) {
+        String buah1 ="anggur";
+        String buah2 ="";
+        System.out.println("buah1 isempty());
+        System.out.println("buah2 isempty());
+        
+    }
+}
